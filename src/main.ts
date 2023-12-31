247b613169f15bbac9546f6466055b7fa2ee5eda
Cyril Vlaminck <vlaminck.cyril@gmail.com>
2023-12-31 22:31:10 +0100

    Cleanup existing templates + Added bootstrap.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 import { createApp } from 'vue'
 import router from './router'
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import SignInView from '@/views/SignInView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,12 +7,12 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      redirect: 'signin'
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
+      path: '/sign-in',
+      name: 'signin',
+      component: SignInView
     }
   ]
 })
