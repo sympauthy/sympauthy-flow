c63bf61b3c037cfbd1169bec1af2c56d926c531d
Cyril Vlaminck <vlaminck.cyril@gmail.com>
2026-04-04 14:43:22 +0200

    Cleaned up unused service.

--- src/services/SignInService.ts
+++
@@ -1,8 +0,0 @@
-export class SignInService {
-  /**
-   * Return the name of the sign-in claims formatted to be displayed to the end-user.
-   */
-  async getSignInClaimNames(): Promise<string> {
-    return 'Login' // FIXME
-  }
-}
